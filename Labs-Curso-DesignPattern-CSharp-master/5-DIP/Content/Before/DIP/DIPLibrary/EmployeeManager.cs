33c8dfad34d01194e1edad389448dbd817530931
LeoFigueredo <leonardofprada@gmail.com>
2019-08-10 19:11:55 -0400

    First Commit

---
+++ Labs-Curso-DesignPattern-CSharp-master/5-DIP/Content/Before/DIP/DIPLibrary/EmployeeManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DIPLibrary
+{
+    public class EmployeeManager 
+    {
+        private readonly List<Employee> _employees = new List<Employee>();
+
+        public void Save(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+    }
+}
---
+++ Labs-Curso-DesignPattern-CSharp-master/5-DIP/Content/Before/DIP/DIPLibrary/EmployeeSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DIPLibrary
+{
+    public class EmployeeSearch
+    {
+        private readonly EmployeeManager employeeSearch;
+
+        public EmployeeSearch(EmployeeManager employeeManager)
+        {
+            this.employeeSearch = employeeManager;
+        }
+
+        public int GetMaleExecutives()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
